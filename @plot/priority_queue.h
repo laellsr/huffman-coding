cdcd5b81575bec66d9d07d5ada259f988f626c78
Pâmela Medeiros <pamedeiross17@gmail.com>
2018-09-21 07:48:56 -0300

    Update priority_queue.h

--- @plot/priority_queue.h
+++ @plot/priority_queue.h
@@ -8,8 +8,8 @@ typedef struct node node;
 typedef struct priority_queue priority_queue;
 
 priority_queue* create_priority_queue();
-int is_empty(priority_queue *pq);
-void enqueue(priority_queue *pq, void *i, int p);
+int is_queue_empty(priority_queue *pq);
+int enqueue(priority_queue *pq, void *i, int p);
 node* dequeue(priority_queue *pq);
 int maximum(priority_queue *pq);
 void print_priority_queue(priority_queue *pq);
