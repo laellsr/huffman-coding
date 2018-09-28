e58bc78d3ac1785342552fecf7267b5e666df0ab
Pâmela Medeiros <pamedeiross17@gmail.com>
2018-09-28 10:23:24 -0300

    Update graph.m

--- @plot/graph.m
+++ @plot/graph.m
@@ -3,14 +3,14 @@ file1 = fopen('comparisons_pq.txt');
 A1 = fscanf(file1,'%d %d\n',[2 Inf]);
 fclose(file1);
 
-n1 = size(A1,2);
+n1 = size(A1, 2);
 
 for i=1:1:n1
-    x1(i) = A1(1,i);
+    x1(i) = A1(1, i);
 end
 
 for i=1:1:n1
-    y1(i) = A1(2,i);
+    y1(i) = A1(2, i);
 end
 
 % fila de prioridade com heap
@@ -18,20 +18,21 @@ file2 = fopen('comparisons_pqh.txt');
 A2 = fscanf(file2,'%d %d\n',[2 Inf]);
 fclose(file2);
 
-n2 = size(A2,2);
+n2 = size(A2, 2);
 
 for i=1:1:n2
-    x2(i) = A2(1,i);
+    x2(i) = A2(1, i);
 end
 
 for i=1:1:n1
-    y2(i) = A2(2,i);
+    y2(i) = A2(2, i);
 end
 
+% gráfico
 figure;
 plot(x1, y1, 'b-', x2, y2,'r-')
-xlim([0 inf]);
-ylim([0 inf]);
+xlim([0 Inf]);
+ylim([0 Inf]);
 grid on;
 xlabel('size');
 ylabel('comparisons');
